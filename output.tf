3f4c4c0a07a33b32fcc049ca85c41001f66dc6b8
Mathieu Duczmanski <mathieu@lamienois.fr>
2020-01-22 14:38:36 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,5 @@
 # nsg outputs
-output "rule_name" {
-  value = "${azurerm_network_security_rule.rule.*.name}"
-}
+output "rules" {
+    value = azurerm_network_security_rule.rule
 
-output "nsg_name" {
-  value = "${azurerm_network_security_rule.rule.*.network_security_group_name}"
 }
