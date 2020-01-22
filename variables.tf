d55411f8754727d8d58ffe61429c56ca399fc1e7
Mathieu Duczmanski <mathieu@lamienois.fr>
2020-01-22 14:40:26 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 # vnet variables
 
 variable "subscription" {
-  description = "The azure subscription"
+  description = "the azure subscription"
   type        = string
   default     = ""
 }
@@ -24,12 +24,29 @@ variable "resource_group_name" {
 
 variable "rules" {
   description = "name, priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix, network_security_group_name"
-  type        = "map"
-  default     = {}
+  type        = map(object({
+    name                         = string
+    priority                     = string
+    direction                    = string
+    access                       = string
+    protocol                     = string
+
+    source_port_range            = string
+    source_port_ranges           = list(any)
+
+    destination_port_range       = string
+    destination_port_ranges      = list(any)
+
+    source_address_prefix      = string
+    source_address_prefixes      = list(any)
+
+    destination_address_prefix   = string
+    destination_address_prefixes = list(any)
+  }))
 }
 
 variable "network_security_group_names" {
   description = "nsg names ti which the rule will be associated"
-  type        = list(string)
-  default     = []
+  type        = string
+  default     = ""
 }
