066132aa9a0a239249e7f65a914e8d74de828c66
Mathieu Duczmanski <mathieu@lamienois.fr>
2020-01-22 12:58:39 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+# vnet variables
+
+variable "subscription" {
+  description = "the azure subscription"
+  type        = "string"
+  default     = ""
+}
+
+variable "location" {
+  description = "One of the Azure region for the resource provisioning"
+  type        = "string"
+  default     = ""
+}
+
+variable "resource_group_name" {
+  description = "Resource groupe name tha will contain various resources"
+  type        = "string"
+  default     = ""
+}
+
+
+
+#### rules
+
+variable "rules" {
+  description = "name, priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix, network_security_group_name"
+  type        = "map"
+  default     = {}
+}
+
+variable "network_security_group_names" {
+  description = "nsg names ti which the rule will be associated"
+  type        = "list"
+  default     = []
+}
+
